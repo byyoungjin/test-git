eea629cd0935cd8e3acbead2b0309d6807f27aac
HYJ <teen14y@naver.com>
2019-04-14 22:09:16 +0900

    second commit

--- gif-search/src/App.js
+++ gif-search/src/App.js
@@ -5,18 +5,18 @@ import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
 export default class App extends Component {
-  
+
   constructor() {
     super();
     this.state = {
       gifs: []
     };
-  } 
+  }
 
   componentDidMount() {
 
   }
-  
+
   performSearch = (query) => {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
       .then(response => {
@@ -26,19 +26,19 @@ export default class App extends Component {
       })
       .catch(error => {
         console.log('Error fetching and parsing data', error);
-      });    
+      });
   }
-  
-  render() { 
+
+  render() {
     console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm onSearch={this.performSearch} />      
-          </div>   
-        </div>    
+            <SearchForm onSearch={this.performSearch} />
+          </div>
+        </div>
         <div className="main-content">
           <GifList data={this.state.gifs} />
         </div>
